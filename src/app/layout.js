ab92158dfa90a330439a75f254c3b50f7487158a
divyanshjain1314 <jain.divyansh3113@gmail.com>
2025-02-08 02:23:53 +0530

    fix bannerSlider.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "tailwindcss";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,12 +16,13 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
-   </html>
+    </html>
   );
 }
---
+++ src/pages/HomeBannerSLider.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Carousel from "react-bootstrap/Carousel";
+
+const BannerSlider = () => {
+  return (
+    <div className="relative overflow-hidden" id="HOME_CAROUSEL">
+      <Carousel fade style={{ height: "100vh" }}>
+        <Carousel.Item>
+          <img
+            className="w-full object-cover"
+            src="https://img.freepik.com/free-photo/loft-home-office-interior-design_53876-143117.jpg?t=st=1738357184~exp=1738360784~hmac=b2eb609092dbbe89e0e96a79bbe1b8278f2f301d9c22ed94d370b0bfa8e99566&w=740"
+            alt=""
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="w-full object-cover"
+            src="https://img.freepik.com/free-photo/yellow-armchair-living-room-with-copy-space_43614-940.jpg?ga=GA1.1.942608024.1721156636&semt=ais_hybrid"
+            alt=""
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="w-full object-cover"
+            src="https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-shelf-near-dining-table_105762-2053.jpg?t=st=1738396572~exp=1738400172~hmac=1dc3ff4d16586a298b58f56eb37954fec9d49874ebee8c9edbb465dd7402326a&w=740"
+            alt=""
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default BannerSlider;
