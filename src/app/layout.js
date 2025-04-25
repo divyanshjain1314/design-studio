450eb70c0cae0261f43049883ad44d078ba4d1fc
divyanshjain1314 <jain.divyansh3113@gmail.com>
2025-04-26 02:39:58 +0530

    new chnge

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "tailwindcss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer position="top-right" autoClose={5000} />
         {children}
       </body>
     </html>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -54,130 +54,123 @@ const Contact = () => {
   };
 
   return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-      />
-      <section className="bg-[#000] pt-28 pb-5 border-b border-[#8E7861]">
-        <div className="grid grid-cols-2 gap-5">
-          <div className="col-span-2 lg:col-span-1">
-            <img
-              src="/h1-img-14.png"
-              alt="Contact Us"
-              className="w-full object-cover"
-            />
-          </div>
-          <div className="col-span-2 lg:col-span-1 flex justify-center flex-col">
-            <h2 className="uppercase text-[#8E7861] text-4xl font-[600] mb-4 px-4 lg:px-0">
-              contact us
-            </h2>
-            <div className="lg:pe-20 px-4 lg:px-0">
-              <form onSubmit={formik.handleSubmit}>
-                <div className="grid grid-cols-2 gap-3 mb-3">
-                  <div className="lg:col-span-1 col-span-2">
-                    <input
-                      type="text"
-                      name="fullName"
-                      className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
-                      placeholder="Full name"
-                      value={formik.values.fullName}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {formik.touched.fullName && formik.errors.fullName && (
-                      <div className="text-red-500 text-sm">
-                        {formik.errors.fullName}
-                      </div>
-                    )}
-                  </div>
-                  <div className="lg:col-span-1 col-span-2">
-                    <input
-                      type="email"
-                      name="email"
-                      className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
-                      placeholder="E-mail"
-                      value={formik.values.email}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {formik.touched.email && formik.errors.email && (
-                      <div className="text-red-500 text-sm">
-                        {formik.errors.email}
-                      </div>
-                    )}
-                  </div>
-                  <div className="lg:col-span-1 col-span-2">
-                    <PhoneInput
-                      country={"in"} // Default country code
-                      value={formik.values.phone}
-                      onChange={handlePhoneChange} // Custom handler for phone input
-                      inputProps={{
-                        name: "phone",
-                        placeholder: "Enter phone number",
-                        required: true,
-                      }}
-                      specialLabel={false} // Hide the label
-                      countryCodeEditable={false} // Disable country code editing
-                      className="w-full bg-transparent border-b py-[2px] focus:outline-none text-white"
-                    />
-                    {formik.touched.phone && formik.errors.phone && (
-                      <div className="text-red-500 text-sm">
-                        {formik.errors.phone}
-                      </div>
-                    )}
-                  </div>
-                  <div className="lg:col-span-1 col-span-2">
-                    <input
-                      type="text"
-                      name="address"
-                      className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
-                      placeholder="Address"
-                      value={formik.values.address}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {formik.touched.address && formik.errors.address && (
-                      <div className="text-red-500 text-sm">
-                        {formik.errors.address}
-                      </div>
-                    )}
-                  </div>
-                </div>
-
-                <div>
-                  <textarea
-                    name="message"
+    <section className="bg-[#000] pt-28 pb-5 border-b border-[#8E7861]">
+      <div className="grid grid-cols-2 gap-5">
+        <div className="col-span-2 lg:col-span-1">
+          <img
+            src="/h1-img-14.png"
+            alt="Contact Us"
+            className="w-full object-cover"
+          />
+        </div>
+        <div className="col-span-2 lg:col-span-1 flex justify-center flex-col">
+          <h2 className="uppercase text-[#8E7861] text-4xl font-[600] mb-4 px-4 lg:px-0">
+            contact us
+          </h2>
+          <div className="lg:pe-20 px-4 lg:px-0">
+            <form onSubmit={formik.handleSubmit}>
+              <div className="grid grid-cols-2 gap-3 mb-3">
+                <div className="lg:col-span-1 col-span-2">
+                  <input
+                    type="text"
+                    name="fullName"
                     className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
-                    placeholder="Message"
-                    rows={3}
-                    value={formik.values.message}
+                    placeholder="Full name"
+                    value={formik.values.fullName}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                   />
-                  {formik.touched.message && formik.errors.message && (
+                  {formik.touched.fullName && formik.errors.fullName && (
                     <div className="text-red-500 text-sm">
-                      {formik.errors.message}
+                      {formik.errors.fullName}
                     </div>
                   )}
                 </div>
-
-                <div className="pt-10">
-                  <button
-                    disabled={isLoading}
-                    type="submit"
-                    className="border-[#8E7861] border text-white px-5 py-2 hover:bg-[#8E7861]"
-                  >
-                    {isLoading ? "Sending..." : "Send Message"}
-                  </button>
+                <div className="lg:col-span-1 col-span-2">
+                  <input
+                    type="email"
+                    name="email"
+                    className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
+                    placeholder="E-mail"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.touched.email && formik.errors.email && (
+                    <div className="text-red-500 text-sm">
+                      {formik.errors.email}
+                    </div>
+                  )}
                 </div>
-              </form>
-            </div>
+                <div className="lg:col-span-1 col-span-2">
+                  <PhoneInput
+                    country={"in"} // Default country code
+                    value={formik.values.phone}
+                    onChange={handlePhoneChange} // Custom handler for phone input
+                    inputProps={{
+                      name: "phone",
+                      placeholder: "Enter phone number",
+                      required: true,
+                    }}
+                    specialLabel={false} // Hide the label
+                    countryCodeEditable={false} // Disable country code editing
+                    className="w-full bg-transparent border-b py-[2px] focus:outline-none text-white"
+                  />
+                  {formik.touched.phone && formik.errors.phone && (
+                    <div className="text-red-500 text-sm">
+                      {formik.errors.phone}
+                    </div>
+                  )}
+                </div>
+                <div className="lg:col-span-1 col-span-2">
+                  <input
+                    type="text"
+                    name="address"
+                    className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
+                    placeholder="Address"
+                    value={formik.values.address}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.touched.address && formik.errors.address && (
+                    <div className="text-red-500 text-sm">
+                      {formik.errors.address}
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div>
+                <textarea
+                  name="message"
+                  className="w-full bg-transparent border-b py-2 focus:outline-none text-white"
+                  placeholder="Message"
+                  rows={3}
+                  value={formik.values.message}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.message && formik.errors.message && (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.message}
+                  </div>
+                )}
+              </div>
+
+              <div className="pt-10">
+                <button
+                  disabled={isLoading}
+                  type="submit"
+                  className="border-[#8E7861] border text-white px-5 py-2 hover:bg-[#8E7861]"
+                >
+                  {isLoading ? "Sending..." : "Send Message"}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
